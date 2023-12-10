4642c28af5936478399be39c66b13dac42b50942
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-12-10 23:47:35 +0530

    updated

--- two.cpp
+++ two.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
+
+
+
 class hero
 {
     int deep;
     char ram;
 
 public:
+    
+    // default costructor
+
+    hero(){
+        cout<<"this is me hero"<<endl;
+    }
+
+    // parametrisez costructor
+    hero(int deep){
+        cout<<"the adress of this: "<<this<<endl;
+        this->deep =deep;
+    }
+
     int getdeep()
     {
         return deep;
@@ -13,17 +29,26 @@ public:
     void sethelth(int h)
     {
         deep = h;
-    }
+    };
 };
+
+
 
 int main()
 {
-    hero h1;
-    h1.sethelth(20);
+    // hero h1(10);
 
-    hero *b = new hero;
-    b->sethelth(50);
+    // cout<<"adress of h1: "<<&h1<<endl;
+    // h1.getdeep();
 
-    cout <<b->getdeep()<< endl;
 
+    hero temp(20);
+    cout<<temp.getdeep()<<endl;
+
+    // h1.sethelth(20);
+
+    // hero *b = new hero;
+    // b->sethelth(50);
+
+    // cout <<b->getdeep()<< endl;
 }
