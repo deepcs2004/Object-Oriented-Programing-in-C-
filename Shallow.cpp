cd5274feacdafa7a29e94dc8a25fbf5ccdf5b1eb
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-12-11 21:12:12 +0530

    updated

---
+++ Shallow.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class hero
+{
+
+    char name;
+    int health;
+    char level;
+
+public:
+    
+    hero(int health, char level){
+        this->health=health;
+        this->level=level;
+    };
+
+    hero(hero &temp)
+    {
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+
+    void print(void){
+        cout<<"the health is : "<<this->health<<endl;
+        cout<<"the level is : "<<this->level<<endl;
+    }
+
+};
+
+int main(){
+
+    hero deep(30, 'a');
+    deep.print();
+
+};
